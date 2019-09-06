228d62bb45f2e353eb8ec13b6cec0020b8696d64
ahmedalord-aa <54982174+ahmedalord-aa@users.noreply.github.com>
2019-09-06 13:04:55 +0300

    aa
    aa

---
+++ checker.php
@@ -0,0 +1,18 @@
+<?php
+while(true)
+
+      {
+system("cat sip.txt");
+echo"PBX Result List \n";
+system("w");
+echo "Load Is \n";
+// current time
+
+
+// sleep for 10 seconds
+sleep (10);
+
+// wake up !
+
+      }
+?>
